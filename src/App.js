d712af0fea16a1e51bf55b80a922484500c0da17
Dima Moruha <trluxsus@outlook.com>
2020-09-12 21:24:43 +0300

    ReactCourse Dialogs

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import MyPosts from "./components/Profile/MyPosts/MyPosts";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 
 const App = () => {
@@ -12,7 +13,10 @@ const App = () => {
     <div className='app-wrapper'>
         <Header />
         <Navbar />
-        <Profile />
+        <div className={'app-wrapper-content'}>
+            <Dialogs />
+        </div>
+        {/*<Profile />*/}
     </div>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src={'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2F4%2F46%2FIMG_Academy_Logo.jpg%2F220px-IMG_Academy_Logo.jpg&f=1&nofb=1'}/>
             </div>
