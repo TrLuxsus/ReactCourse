2a0f4df0c4b3140412029565f877025be29c8d9f
Dima Moruha <trluxsus@outlook.com>
2020-09-13 12:57:46 +0300

    ReactCourse
    Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import s from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Gotama
+                </div>
+                <div className={s.dialog}>
+                    Alesha
+                </div>
+                <div className={s.dialog}>
+                    Sashko
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    H
+                    </div>
+                <div className={s.message}>
+                    Who are you?
+                </div>
+                <div className={s.message}>
+                    TT
+                </div>
+            </div>
         </div>
     )
 }
